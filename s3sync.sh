5c56993f2312063ffedaf6d612d19e9a1b6b73c1
James Salvatore <james.salvatore@abovelending.com>
2026-01-12 11:04:03 -0600

    chore: attempts at requirements;

---
+++ s3sync.sh
@@ -0,0 +1,2 @@
+echo "Synchronizing files to prod bucket s3://above-lending-prod-airflow-3/dags ..."
+aws s3 sync ./images/airflow/3.0.6/dags "s3://above-lending-prod-airflow-3/dags" --delete --exact-timestamps --exclude "*.DS_Store" --exclude "*.pyc" "$@"
