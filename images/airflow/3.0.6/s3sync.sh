7ca36648eaea6ac3718007600e8724527a24add4
James Salvatore <james.salvatore@abovelending.com>
2026-01-27 13:49:48 -0600

    chore: update sync scripts;

--- images/airflow/3.0.6/s3sync.sh
+++ images/airflow/3.0.6/s3sync.sh
@@ -1,2 +1,2 @@
-echo "Synchronizing files to prod bucket s3://above-lending-prod-airflow-3-test/dags ..."
-aws s3 sync ./dags "s3://above-lending-prod-airflow-3-test/dags" --delete --exact-timestamps --exclude "*.DS_Store" --exclude "*.pyc" "$@"
+echo "Synchronizing files to prod bucket s3://above-lending-prod-airflow-3/dags ..."
+aws s3 sync ./dags "s3://above-lending-prod-airflow-3/dags" --delete --exact-timestamps --exclude "*.DS_Store" --exclude "*.pyc" "$@"
